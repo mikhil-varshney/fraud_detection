a3aceecd0e70fd4d818c958c3260a1355cacae9d
mikhil-varshney <mikhil.varshney@gmail.com>
2021-10-31 16:36:06 +0530

    added 3 model for ML code

--- Ml_Model.py
+++ Ml_Model.py
@@ -1,2 +1,12 @@
 
 print("adding all ML code related to it")
+
+def model1():
+    print("adding code for ML model 1")
+
+
+def model2():
+    print("adding code for ML model 2")
+
+def model3():
+    print("adding code for ML model 3")
